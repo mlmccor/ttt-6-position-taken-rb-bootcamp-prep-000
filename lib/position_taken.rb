431024ee24f5de2b5cb237fd9b94765bb494a70f
Matthew McCormack <mmccormack1182@gmail.com>
2018-08-17 21:12:14 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,4 @@ def position_taken?(board,index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
   end
-  if board[index] == "X" || board[index] == "O"
-    true
-  end
 end
